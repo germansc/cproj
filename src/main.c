a6f288b274aeac47cc145f46e5ab959f0003033b
germansc <germanscillone@gmail.com>
2023-11-14 10:28:44 -0300

    Format fixes in main.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
-/*
- * File:   main.c
- * Author: germansc
+/* ***************************************************************************
+ *   Project: C Project Template
+ *      file: main.c
+ *    author: germansc
+ *   version: 1.0
  *
- * Simple C template project.
- */
+ * Copyright (c) 2023 AUTHOR_TAG - All Rights Reserved.
+ *
+ * ************************************************************************* */
 
 #include <stdio.h>
 
+/**
+ * @brief This is the main entry point of the program.
+ *
+ * @param argc Number of arguments given to the command on execution.
+ * @param argv List of arguments given to the command.
+ * @return always 0.
+ */
 int main(int argc __attribute__((unused)), char const *argv[] __attribute__((unused)))
 {
     printf("Generic C Project Template.\n");
